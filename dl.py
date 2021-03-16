8c0f606042fc42d2b4f2aa0f9de8ddb589bfe2bf
Quizznor <paul.filip@protonmail.com>
2021-03-16 15:12:42 +0100

    Streamline directory management

--- dl.py
+++ dl.py
@@ -7,15 +7,17 @@ import youtube_dl
 import gid
 from mutagen.id3 import ID3, TIT2, TPE1, TALB, APIC
 
+target_path = "/home/quizznor/music"
+
 # Download the mp3 file
 ydl_arguments = {"--extract-audio",
                  "--add-metadata",
                  "--audio-format mp3",
-                 "--output '/home/quizznor/music/%(id)s.%(ext)s'"}
+                 f"--output '{target_path}/%(id)s.%(ext)s'"}
 
 print("\nDOWNLOADING " + sys.argv[1])
 os.system(f"youtube-dl {' '.join(ydl_arguments)} {sys.argv[1]}")
-audio_path = "/home/quizznor/music/" + str([file for file in os.listdir("/home/quizznor/music") if file.endswith(".mp3")][0])
+audio_path = f"{target_path}/" + str([file for file in os.listdir(f"{target_path}") if file.endswith(".mp3")][0])
 
 
 # Fiddle with metadata
@@ -41,7 +43,7 @@ for tag in ["TDRC","TXXX:description","TXXX:comment","TXXX:purl","TSSE"]:
 
 
 # Check if song already exists
-all_songs = open("/home/quizznor/music/lib").readlines()
+all_songs = open(f"{target_path}/lib").readlines()
 if "{} - {}\n".format(NAMES[0],NAMES[1]) in all_songs:
     prompt = input("\nSong already in your library, continue? ")
     if prompt != "y":
@@ -49,22 +51,21 @@ if "{} - {}\n".format(NAMES[0],NAMES[1]) in all_songs:
         sys.exit("Goodbye!\n")
 
 # Add album cover art
-img_path_should = f"/home/quizznor/music/coverart/{str(NAMES[0]).replace(' ','_').lower()}-{str(NAMES[2]).replace(' ','_').lower()}.png"
+img_path_should = f"{target_path}/coverart/{str(NAMES[0]).replace(' ','_').lower()}-{str(NAMES[2]).replace(' ','_').lower()}.png"
 if not os.path.isfile(img_path_should):
-    gid_arguments = {"keywords": f"{NAMES[0]} {NAMES[1]} album cover",
-                 "format": "png",
+    gid_arguments = {"keywords": f"{NAMES[0]} {NAMES[2]} album cover",
                  "limit":1,
                  "no_directory":True,
                  "prefix":f"{NAMES[0]} {NAMES[1]}",
-                 "output_directory":"/home/quizznor/music/coverart/",
+                 "output_directory":f"{target_path}",
                  "no_numbering":True,
-                 "silent_mode":True,
-                 "aspect_ratio":"wide"}
+                 "silent_mode":True,}
 
     response = gid.googleimagesdownload()
     absolute_image_paths = response.download(gid_arguments)
-    img_path_is = [file for file in os.listdir("/home/quizznor/music/coverart") if file.startswith(f"{NAMES[0]} {NAMES[1]}")][0]
-    os.system(f"mv '/home/quizznor/music/coverart/{img_path_is}' {img_path_should}")
+    print(f"{target_path}/coverart")
+    img_path_is = [file for file in os.listdir(f"{target_path}") if file.startswith(f"{NAMES[0]} {NAMES[1]}")][0]
+    os.system(f"mv '{target_path}/{img_path_is}' {img_path_should}")
 
 audio.add(APIC(mime='image/jpeg',type=3,desc=u'Cover',data=open(f"{img_path_should}",'rb').read()))
 audio.save()
@@ -72,12 +73,12 @@ audio.save()
 
 # Update library and move song
 print("\nUPDATING LIBRARY AND MOVING SONG TO APPROPRIATE FOLDER")
-with open("/home/quizznor/music/lib", "a") as library:
+with open(f"{target_path}/lib", "a") as library:
     library.write("{} - {}\n".format(NAMES[0],NAMES[1]))
 
-if not os.path.isdir(f"/home/quizznor/music/artists/{str(NAMES[0]).replace(' ','_').lower()}"):
-    os.system(f"mkdir /home/quizznor/music/artists/{str(NAMES[0]).replace(' ','_').lower()}")
+if not os.path.isdir(f"{target_path}/artists/{str(NAMES[0]).replace(' ','_').lower()}"):
+    os.system(f"mkdir {target_path}/artists/{str(NAMES[0]).replace(' ','_').lower()}")
 
-os.system(f"mv {audio_path} /home/quizznor/music/artists/{str(NAMES[0]).replace(' ','_').lower()}/{str(NAMES[1]).replace(' ','_').lower()}.mp3")
+os.system(f"mv {audio_path} {target_path}/artists/{str(NAMES[0]).replace(' ','_').lower()}/{str(NAMES[1]).replace(' ','_').lower()}.mp3")
 
 print("Track sucessfully added to library!\n")
